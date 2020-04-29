907c563a783ec30e195f6631916b15b9ad3b2cd9
NderituMwanu <mwanugerald@gmail.com>
2020-04-30 02:06:40 +0300

    Source and Article classes updated

--- app/config.py
+++ app/config.py
@@ -1,17 +1,23 @@
-import os
+import os # this will help the application work on various operating systems
 
 class Config:
     '''
-    General configuration parent class
+    Parent class for general configuration
     '''
-    NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    OURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
    	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
 
 class ProdConfig(Config):
+    '''
+    Production stage configuration
+    '''
     pass
 
-
 class DevConfig(Config):
+    '''
+    development stage configration
+    '''
+
     DEBUG = True
 
 config_options = {
