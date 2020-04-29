b35bda82385523995356af73974f00df61a5fa29
NderituMwanu <mwanugerald@gmail.com>
2020-04-30 02:07:27 +0300

    source and article classes updated

--- app/models.py
+++ app/models.py
@@ -2,27 +2,27 @@ class Source:
     '''
     Source class to define Source Objects
     '''
-    def __init__(self, id, name, description, url, category, language, country):
-        self.id =  id
+    def __init__(self,id,name,description, url,category,language):
         self.name = name
+        self.id =  id
         self.description = description
         self.url = url
         self.category = category
         self.language = language
-        self.country = country
+       
 
 class Article:
     '''
     Article class to define Article Objects
     '''
-    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt, content):
-        self.id = id
+    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt):
         self.name = name
+        self.id = id
         self.author =  author
         self.title = title
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
-        self.content = content
+    
 
